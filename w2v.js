e089386f168d580e3ee0f869e1ce7c5f2085af41
Michaël <afmichael73@gmail.com>
2022-08-04 16:14:20 +0300

    -- done most accurate naive impl

--- w2v.js
+++ w2v.js
@@ -1,32 +1,7 @@
 const {ShioriWord2Vec, WordVector} = require('./core/ShioriWord2Vec');
 const sw2v = new ShioriWord2Vec ();
 
-sw2v.loadVectorsFromFile ('./trained/shiori-w2v/shakespear-vectors.json');
-// sw2v.loadTextFromFile (
-//     './datas/gutenberg-books/t8.shakespeare.txt',
-//     './datas/exclude.txt'
-// );
-// sw2v.max_vec_dimension = 2500;
-// let current_mult = 0, state = null;
-// sw2v.trainOptimally (1, (msg, n_current, total) => {
-//     const p = Math.floor (100 * n_current / total);
-//     const chunk_size = 5;
-//     const k = Math.floor (p / chunk_size);
-//     const r = p - k * chunk_size;
-//     const curr_state = msg.split(':')[0];
-//     if (curr_state != state) {
-//         state = curr_state;
-//         current_mult = 0;
-//     }
-//     if (k > current_mult && r == 0) {
-//         console.log ('Training : '+ p + '%', 'status ' + msg, n_current + '/' + total);
-//         current_mult = k;
-//     }
-// });
-
-// sw2v.saveVectorsTo ('./shakespear-vectors.json')
-
-console.log('Infos :: ', sw2v.infos());
+sw2v.loadVectorsFromFile ('./trained/shiori-w2v/t8.shakespeare-vector.json');
 
 function vec (w) {
     return sw2v.word2vec (w);
